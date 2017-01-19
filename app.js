27b0ffac06cc05f8327fbf44393dfeb3ee7b6973
bluemix trial <bluemix.trial.101@gmail.com>
2017-01-19 09:56:35 -0600

    1

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
- var ws = require('nodejs-websocket')
-  
- // Secure Bluemix server from Scream server example: "hi" -> "HI!!!" 
- var server = ws.createServer( function (conn) {
-     console.log("New connection")
-     if (conn.headers["x-forwarded-proto"] === "http" ) {
-         conn.sendText("Only Secure Web Sockets are allowed")
-         conn.close(1002, "Only Secure Web Sockets are allowed")
-     }
-     
-     conn.on("text", function (str) {
-         console.log("Received "+str)
-         conn.sendText(str.toUpperCase()+"!!!")
-     })
-     
-     conn.on("close", function (code, reason) {
-         console.log("Connection closed")
-     })
- }).listen(3000)
+var cfenv = require( 'cfenv' );  
+var express = require( 'express' );  
+var http = require( 'http' );  
+var ws = require( 'ws' );
+
+// Environment
+var environment = cfenv.getAppEnv();
+
+// Web
+var app = express();
+
+// Static
+app.use( '/', express.static( 'public' ) );
+
+// Sockets
+var server = http.createServer();  
+var sockets = new ws.Server( {  
+  server: server
+} );
+
+// Listeners
+sockets.on( 'connection', function( client ) {  
+  // Debug
+  console.log( 'Connection.' );
+
+  // Echo messages to all clients
+  client.on( 'message', function( message ) {
+    for( var c = 0; c < sockets.clients.length; c++ ) {
+      sockets.clients[c].send( message );   
+    }
+  } );
+} );
+
+// Start
+server.on( 'request', app );  
+server.listen( environment.port, function() {  
+  console.log( environment.url );
+} );
